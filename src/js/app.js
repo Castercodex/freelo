dce4706c19aca07f4563c8b8ff18036b11a53630
Castercodex <edwardberth1@gmail.com>
2021-01-16 18:29:03 +0100

    v1.0

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+const burger = document.querySelector(".hamburger");
+let nav = document.querySelector(".nav-links");
+let cancel = document.querySelector(".cancel");
+let navShow = document.querySelector(".nav-show");
+let navCircle = document.querySelector(".nav-circle");
+let navBar = document.querySelector("nav");
+
+if (window.outerWidth > 768) {
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 70) {
+      navBar.classList.add("nav");
+    } else {
+      navBar.classList.remove("nav");
+    }
+  });
+}
+
+burger.onclick = () => {
+  nav.classList.toggle("nav-mobile");
+  cancel.classList.toggle("cancel-view");
+  navShow.classList.toggle("cancel");
+};
+
+navCircle.onclick = () => {
+  navCircle.classList.toggle("active");
+};
